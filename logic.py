f32e1e253b2a31efdb11b2a00bdbfce916fbed4d
groupe05b <t.j.l@hotmail.fr>
2012-03-30 17:50:17 +0200

    modifie premiere du nom

---
+++ logic.py
@@ -0,0 +1,7 @@
+""" tata """
+
+def and_(a, b):
+    return None
+    
+def or_(a, b):
+    return None
---
+++ operators.py
@@ -0,0 +1,8 @@
+""" tata """
+
+def add(a, b):
+    return None
+    
+def mul(a, b):
+    return None
+
